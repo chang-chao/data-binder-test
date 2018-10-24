01489126c536ea77e1d100d6eb168438543ae86b
chang-chao <chang-chao@hotmail.com>
2018-10-25 00:29:15 +0900

    OK

--- src/main/java/me/changchao/spring/databindertest/DataBinderTestApplication.java
+++ src/main/java/me/changchao/spring/databindertest/DataBinderTestApplication.java
@@ -12,7 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.context.properties.bind.PropertySourcesPlaceholdersResolver;
+import org.springframework.boot.context.properties.source.ConfigurationProperty;
+import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
 import org.springframework.boot.context.properties.source.ConfigurationPropertyNameAliases;
+import org.springframework.boot.context.properties.source.ConfigurationPropertyState;
 import org.springframework.boot.context.properties.source.IterableConfigurationPropertySource;
 import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -40,23 +43,47 @@ public class DataBinderTestApplication {
 				mapSource.putAll(((MapPropertySource) propertySource).getSource());
 			}
 		}
+		ConfigurationPropertyName fooPropertyName = ConfigurationPropertyName.of("foo");
 
+		IterableConfigurationPropertySource fooPropertySource = mapSource
+				.filter(a -> a != null && (fooPropertyName.isAncestorOf(a) || fooPropertyName.equals(a)));
 		ConfigurationPropertyNameAliases aliases = new ConfigurationPropertyNameAliases();
+
+		aliases.addAliases("foo.bar", "foo.other-name");
+
+		aliases.addAliases("foo.bar[0]", "foo.other-name[0]");
+		aliases.addAliases("foo.bar[1]", "foo.other-name[1]");
+
+//		aliases.addAliases("foo.other-name[0]", "foo.bar[0]");
+//		aliases.addAliases("foo.other-name[1]", "foo.bar[1]");
+
+
 		aliases.addAliases("foo.other-name[0].name", "foo.bar[0].name");
 		aliases.addAliases("foo.other-name[1].name", "foo.bar[1].name");
-		aliases.addAliases("foo.other-name", "foo.bar");
-		// aliases.addAliases("foo.other-name[n]", "foo.bar[n]");
-		IterableConfigurationPropertySource withAliases = mapSource.withAliases(aliases);
+
+//		aliases.addAliases("foo.bar[0].name", "foo.other-name[0].name");
+//		aliases.addAliases("foo.bar[1].name", "foo.other-name[2].name");
+
+		IterableConfigurationPropertySource withAliases = fooPropertySource.withAliases(aliases);
+		ConfigurationPropertyState contains = withAliases.containsDescendantOf(ConfigurationPropertyName.of("foo.bar"));
+		ConfigurationProperty configurationProperty = withAliases
+				.getConfigurationProperty(ConfigurationPropertyName.of("foo.bar"));
 
 		Consumer<PropertyEditorRegistry> propertyEditorInitializer = applicationContext
 				.getBeanFactory()::copyRegisteredEditorsTo;
+
 		Binder binder = new Binder(Collections.singleton(withAliases), new PropertySourcesPlaceholdersResolver(env),
 				conversionService.orElse(null), propertyEditorInitializer);
 
 		BindResult<Foo> bind = binder.bind("foo", Foo.class);
-		Map<String, Object> underlyingSource = (Map<String, Object>) mapSource.getUnderlyingSource();
-		System.out.println(underlyingSource.keySet());
-		return bind.get();
+
+		Iterator<ConfigurationPropertyName> names = withAliases.iterator();
+		names.forEachRemaining(a -> System.out.println(a));
+
+		Foo foo = bind.get();
+		System.out.println("foo=" + foo);
+		return foo;
+		// return null;
 	}
 
 }
